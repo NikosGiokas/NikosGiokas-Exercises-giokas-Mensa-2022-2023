55735f7a5de6e06fe6f60c05bc5c719d66fe8273
NikosGiokas <115407659+NikosGiokas@users.noreply.github.com>
2022-10-14 12:43:40 +0200

    Update Askisi-3.py

--- Askisi-3.py
+++ Askisi-3.py
@@ -2,11 +2,11 @@
 num1 = input("Give me a number: ")
 num2=input("give me another number: ")
 num3=input("give me another number: ")
-if num1 > num2 and num3: #exeis lathos edo
+if num1 > num2 and num1>num3: #exeis lathos edo
     print(str(num1)+" is bigger")
-if num2 > num1 and num3:#exeis lathos edo
+if num2 > num1 and num2>num3:#exeis lathos edo
     print(str(num2)+" is bigger")
-if num3 > num2 and num1:#exeis lathos edo
+if num3 > num2 and num3 >num1:#exeis lathos edo
     print(str(num3)+" is bigger")
 if num3 == num2 and num1 == num2:
-    print("they are equal")
+    print("they are all equal")
