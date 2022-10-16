a0f6b0da642a1ab312fc886412d47b9616c09f44
NikosGiokas <nikosgiokas@icloud.com>
2022-10-16 16:55:21 +0200

    Create myOwnFirstPythonGame.py

---
+++ myOwnFirstPythonGame.py
@@ -0,0 +1,26 @@
+
+str1 = "some"
+str2 = "thing"
+str3 = " weird"
+str4 = " studios"
+for i in range(0,5,1):
+    print(" ///// ")
+print(str1+str2+str3+str4)
+for i in range(0,5,1):
+
+    print(" ///// ")
+# /\
+# ||
+# ||
+#start code
+print("RAFT survival")
+input("press enter to continue... ")
+input("you find yourself in a sticky situation...")
+input("stranded on a raft...")
+input("now you need to survive...")
+input("by finding stuff, crafting stuff and eventually...")
+input("consuming them.")
+# /\
+# ||
+# ||
+#intro code
