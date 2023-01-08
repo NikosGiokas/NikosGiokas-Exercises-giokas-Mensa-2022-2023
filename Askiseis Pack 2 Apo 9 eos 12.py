86f94949ab1b673b24d786f14d00143b139eb71b
NikosGiokas <115407659+NikosGiokas@users.noreply.github.com>
2023-01-08 16:36:07 +0100

    teleioma askiseon

--- Askiseis Pack 2 Apo 9 eos 12.py
+++ Askiseis Pack 2 Apo 9 eos 12.py
@@ -8,8 +8,6 @@
 #  Date  19.11.2022    
 #    
 # #
-
-
 # askisi 9
 
 #paidia = int(input("Dose aritho paidion: "))
@@ -26,8 +24,6 @@
 #        bonus = 0.03
 #    return print("o misthos sou einai: ",misthos + bonus * misthos / 10 )
 #epidoma()
-
-
 # askisi 10
 # Να γράψετε πρόγραμμα σε Python που θα δέχεται τον προφορικό και το γραπτό βαθμό 
 # ενός μαθητή και θα υπολογίζει το μέσο όρο του και εξάγει αποτέλεσμα προαγωγής. 
@@ -55,3 +51,49 @@
 # θα υπολογίζει και θα εμφανίζει το κόστος του εισιτηρίου του.
 
 
+#spectatorAge = int(input("please give me your age: "))
+
+def price():
+    global spectatorAge
+    cost = 0
+    if spectatorAge < 18:
+        cost = 10
+    elif spectatorAge >= 18 and spectatorAge < 26:
+
+        cost = 15*0.75
+    else:
+
+
+        cost = 15
+    print("it will cost",cost,"€")
+#price()
+
+
+#askisi 12
+
+#Να γραφτεί πρόγραμμα σε γλώσσα Python που να υπολογίζει το μισθό ενός εργαζομένου,
+# ο οποίος δουλεύει με σύμβαση ωρομισθίου σε μια εταιρεία. Να διαβάζει το όνομά του,
+# τις ώρες εργασίας του μήνα, το ωρομίσθιό του και αν είναι έγγαμος ή άγαμος. 
+# Οι κρατήσεις που του γίνονται, εξαρτώνται από το ύψος του μισθού. 
+# Αν ο μισθός είναι μέχρι 1000€ το μήνα, έχει 15% κρατήσεις, ενώ διαφορετικά έχει 25% κρατήσεις. 
+# Επίσης, αν είναι έγγαμος, έχει ένα επίδομα 50€.
+
+
+workerName = input("give me the name of the worker: ")
+workerHours = input("give me the hours of the worker: ")
+workerSalary = int(input("give me the salary of the worker: "))
+isWorkerMarried = int(input("give me a 1 if the worker is married: "))
+deduction = 0
+def workerDeduction():
+    global workerSalary
+    if workerSalary > 1000:
+        deduction = 0.75
+    else:
+        deduction = 0.85
+    
+    workerSalary*=deduction
+
+    if isWorkerMarried == 1:
+        workerSalary+=50
+    print(workerSalary)
+workerDeduction()
